fef0de98e6c1bfaa5a6e1ea5e501a456ab4b0003
23730237-Tin <s4m0y3d@gmail.com>
2024-09-15 23:07:41 +0700

    commit graphic

---
+++ GamePanel.java
@@ -0,0 +1,17 @@
+GameGraphics.java
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+
+public class GameGraphics {
+    final int SCREEN_WIDTH = 600;
+    final int SCREEN_HEIGHT = 600;
+    final int UNIT_SIZE = 25;
+
+    public void draw(Graphics g, int[] x, int[] y, int bodyParts, int appleX, int appleY, boolean running) {
+        if (running) {
+            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            }
