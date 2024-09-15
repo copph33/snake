814bde8691dabbd1398dc7a2bd4f515cbe0c4e99
23730237-Tin <s4m0y3d@gmail.com>
2024-09-15 23:09:38 +0700

    commit graphic on branch-tin

--- GamePanel.java
+++ GamePanel.java
@@ -14,4 +14,8 @@ public class GameGraphics {
             for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
                 g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
                 g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-            }
+            
+	    }
+	     // Vẽ táo
+            g.setColor(Color.RED);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
